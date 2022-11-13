763759ab419bb4961a57b669b5da4e7b43a43e62
mohamed12370 <mohamedmostafa12370@gmail.com>
2022-11-13 13:00:56 +0200

    add middleware handle error

--- build/index.js
+++ build/index.js
@@ -4,11 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+app.use((0, morgan_1.default)('common'));
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.json({
         message: 'Home',
+    });
+});
+app.use((_req, res) => {
+    res.status(404).json({
+        message: 'this reote is not found plz respest the api route',
     });
 });
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
 
 const app: Application = express();
-
 const port = process.env.PORT || 3000;
+
+app.use(morgan('common'));
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'Home',
+  });
+});
+
+app.use((_req: Request, res: Response) => {
+  res.status(404).json({
+    message: 'this reote is not found plz respest the api route',
   });
 });
 
